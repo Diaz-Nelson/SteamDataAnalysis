2bf798bb976b55ac9f3fecf7c5131d1de3c19b20
Nelson-Diaz <juniordiaz1059@gmail.com>
2025-07-30 13:18:15 -0500

    added plotly to the requirements

--- data_collector_script.py
+++ data_collector_script.py
@@ -1,9 +1,6 @@
 from Functions import data_funcs
 from datetime import datetime
 
-# Gets the time as soon as the program is ran to be used later to calculate the time taken to retrieve all the data from steam charts, Steam API, and cleaning it.
-start = datetime.now()
-
 # Retrives the data for today from all sources and saves the game data along with various details on the pipeline
 game_data,details_failed,game_failed,tags_failed,dateCollected = data_funcs.get_game_data(8)
 
